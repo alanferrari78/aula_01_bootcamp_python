ccc0f60ce3e7f1cb5422e1ed8c3d4b78bf3eeff5
Alan Ferrari <alanfolha@gmail.com>
2024-08-06 23:29:07 -0300

    atualizacao do main

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello Word")
+print("Hello Word");
+print("Alguma coisa");
+print(3+2);
+print("Olá"+" "+"turma!");
+print(True);
+
+# print("Olá " + input ("Digite teu nome: ")+"! Seja bem vindo!");
+
+# Crie um programa que o usuário digite o seu nome e retorne o numero de caracters
+
+#nome = (input ("Digite teu nome: "));
+#1print(len(nome));
+
+# Digite dois valores e retorne a soma deles
+
+val1 = (input ("Digite um numeto: "));
+val2 = (input ("Digite outro numero: "));
+print(int(val1)+int(val2));
+
+
+numero = 3 # int
+numero_decimal = 3.14 #float
+nome = "Alan" # String
+verdadeiro = True # or False Bool
+
+print(type(numero))
+
